912b40195732a591a5982c615ec95dfb587f8218
Monique <moniquemoledo@hotmail.com>
2015-11-19 22:08:09 -0700

    Create message Entity and Message Repository

---
+++ src/main/java/com/springapp/mvc/repository/MessageRepository.java
@@ -0,0 +1,19 @@
+package com.springapp.mvc.repository;
+
+import com.springapp.mvc.enteties.Connection;
+import com.springapp.mvc.enteties.Message;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by Monique on 11/19/2015.
+ */
+public interface MessageRepository <T, ID extends Serializable> extends CrudRepository<Connection, Long> {
+
+    @Query("select m from messages m where m.user_id = ?1 and m.status=false")
+    List<Message> findByUserId(Long userId);
+
+}
